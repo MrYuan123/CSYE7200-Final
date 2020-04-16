2f9caf192163191ba0899a2b2917ba655a814ba0
RAN ZHOU <zhou.ra@husky.neu.edu>
2020-04-15 21:12:11 -0400

    edit portal page, remove some field

--- WebServer/app.py
+++ WebServer/app.py
@@ -3,16 +3,6 @@ from flask import render_template
 import dao, calculation
 import pymysql, json
 
-# cache = Cache()
-#
-# config = {
-#   'CACHE_TYPE': 'redis',
-#   'CACHE_REDIS_HOST': '127.0.0.1',
-#   'CACHE_REDIS_PORT': 6379,
-#   'CACHE_REDIS_DB': '',
-#   'CACHE_REDIS_PASSWORD': ''
-# }
-
 app = Flask(__name__)
 
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -48,18 +38,6 @@ def getResult(stock_name):
     print(result)
     index = obj.getfunc("index")
 
-    # volume = float(redisobj.get("appl-v"))
-    #int.from_bytes(testBytes, byteorder='big')
-    # int_res = [float(x) for x in res]
-    # result = []
-    # for item in stock_dict:
-    #     try:
-    #         rvalue = redisobj.get(item)
-    #         result.append(rvalue)
-    #     except:
-    #         print("Error!")
-    # 执行SQL语句
-    # print(int_res)
     return render_template('result.html', tech_index=float(index), portfolio=result, stock_name=stock_name)
 
 
